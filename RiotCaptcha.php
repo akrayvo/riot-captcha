0d7033919fdb89fb05c9b14fbdc0959ea800bcad
adam k <krayvo@gmail.com>
2022-03-11 13:42:44 -0700

    random string and key generated

--- RiotCaptcha.php
+++ RiotCaptcha.php
@@ -18,6 +18,11 @@ class RiotCaptcha
 
     private static $string = '';
 
+    private static $validCharacters = 'ACDEFGHJKLMNPRTVWXYZ234679';
+
+    private static $stringLength = 5;
+    private static $keyLength = 12;
+
     /**
      * DESCRIPTION HERE
      */
@@ -52,6 +57,53 @@ class RiotCaptcha
         if (!empty($captchaTextFilePath)) {
             self::setCaptchaTextFilePath($captchaTextFilePath);
         }
+        self::createRandomString();
+        self::createRandomKey();
+        self::saveToFile();
+    }
+
+    /**
+     * DESCRIPTION HERE
+     */
+    public static function saveToFile()
+    {
+    }
+
+    /**
+     * DESCRIPTION HERE
+     */
+    private static function getRandomString($characters, $length) {
+        $str = '';
+        $maxRand = strlen($characters) - 1;
+        for ($x = 1; $x <= $length; $x++) {
+            $rand = rand(0, $maxRand);
+            $str .= substr($characters, $rand, 1);
+        }
+        return $str;
+    }
+
+    /**
+     * DESCRIPTION HERE
+     */
+    private static function createRandomString() {
+        self::$string = self::getRandomString(
+            self::$validCharacters,
+            self::$stringLength
+        );
+    }
+
+    /**
+     * DESCRIPTION HERE
+     */
+    private static function createRandomKey()
+    {
+        $validCharacters = '0123456789'.
+            'abcdefghijklmnopqrstuvwxyz'.
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        self::$key = self::getRandomString(
+            $validCharacters,
+            self::$keyLength
+        );
     }
 
     /**
